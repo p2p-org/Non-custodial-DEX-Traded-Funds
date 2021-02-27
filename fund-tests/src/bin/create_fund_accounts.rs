a38bb1068615c359cc7f1b2092b51551dae8afb7
Alexander XX <freecoder.xx@gmail.com>
2021-02-27 21:41:17 +0300

    Use temporarily created accounts

--- fund-tests/src/bin/create_fund_accounts.rs
+++ fund-tests/src/bin/create_fund_accounts.rs
@@ -13,20 +13,69 @@ use solana_sdk::{
 fn main() -> Result<()> {
     let fund_program_id = Pubkey::from_str("HWEaSAnjKNADwA2ZopatwCM5BqrnMCPQBKAphf1s6XNW")?;
 
-    let initializer_account =
+    let payer_account =
         read_keypair_file("../../solana/alice.json").map_err(|err| anyhow!("Read keypair error: {}", err))?;
 
     let rpc_client = RpcClient::new_with_commitment("http://localhost:8899".into(), CommitmentConfig::confirmed());
     let mut client = Client {
         client: rpc_client,
-        payer: Keypair::from_bytes(&initializer_account.to_bytes()).unwrap(),
+        payer: payer_account,
     };
 
-    let initializer_x_token_account_pubkey = Pubkey::from_str("6F23oWcsyp7D5RkFvi4fha9oUZe1KbK1KvsmDrVS6MeL")?;
-    let initializer_y_token_account_pubkey = Pubkey::from_str("Ho1Y6G3BwMHjxvFw2AVUvcPk9AcyiR6TsR3d72pLu9Me")?;
+    let initializer_account = Keypair::new();
+    println!(
+        "initializer_account: {}, {}",
+        initializer_account.pubkey(),
+        initializer_account.to_base58_string()
+    );
 
-    let x_token_mint_account_pubkey = Pubkey::from_str("ENZSTnpp3FntMj9ApgypoaHY6ZJs8iosyCeFnJj81kpp")?;
-    let y_token_mint_account_pubkey = Pubkey::from_str("5ZTTw1mmjcBzTYB5igjoxUCFAyw8jYczdDgfxWqzV5CS")?;
+    // Create assets
+    let x_token_mint = token::create_token(&mut client, &initializer_account.pubkey(), 2);
+    println!(
+        "x_token_mint: {}, {}",
+        x_token_mint.pubkey(),
+        x_token_mint.to_base58_string()
+    );
+    let y_token_mint = token::create_token(&mut client, &initializer_account.pubkey(), 0);
+    println!(
+        "y_token_mint: {}, {}",
+        y_token_mint.pubkey(),
+        y_token_mint.to_base58_string()
+    );
+
+    // Mint to user
+    let initializer_x_token_account =
+        token::create_account(&mut client, &initializer_account.pubkey(), &x_token_mint.pubkey());
+    println!(
+        "initializer_x_token_account: {}, {}",
+        initializer_x_token_account.pubkey(),
+        initializer_x_token_account.to_base58_string()
+    );
+
+    let initializer_y_token_account =
+        token::create_account(&mut client, &initializer_account.pubkey(), &y_token_mint.pubkey());
+    println!(
+        "initializer_y_token_account: {}, {}",
+        initializer_y_token_account.pubkey(),
+        initializer_y_token_account.to_base58_string()
+    );
+
+    token::mint_to(
+        &mut client,
+        &initializer_account,
+        &x_token_mint.pubkey(),
+        &initializer_x_token_account.pubkey(),
+        1000,
+        2,
+    );
+    token::mint_to(
+        &mut client,
+        &initializer_account,
+        &y_token_mint.pubkey(),
+        &initializer_y_token_account.pubkey(),
+        200,
+        0,
+    );
 
     let fund_name = "Test fund";
 
@@ -63,8 +112,7 @@ fn main() -> Result<()> {
     );
     // let fund_token_account = Keypair::from_base58_string("GS83JUgS4Ni36SJLWdB9zVg1xSr4WEzUVxMpt5388NuneDRCuuVqiHHko1KiTqvTFUhWDoFumJJpyoRjrJZ5MDE");
 
-    let fund_x_token_vault_account =
-        token::create_account(&mut client, &fund_vault_authority, &x_token_mint_account_pubkey);
+    let fund_x_token_vault_account = token::create_account(&mut client, &fund_vault_authority, &x_token_mint.pubkey());
     println!(
         "fund_x_token_vault_account: {}, {}",
         fund_x_token_vault_account.pubkey(),
@@ -72,8 +120,7 @@ fn main() -> Result<()> {
     );
     // let fund_x_token_vault_account = Keypair::from_base58_string("1LxFNC6cuWzg6P8Emz6a7VTkNNLrgY1LA2d36i9KfJXH6nCLBG91vvGhx6gX43jHYwnEmTt1RMRFRy6hKgSkdYc");
 
-    let fund_y_token_vault_account =
-        token::create_account(&mut client, &fund_vault_authority, &y_token_mint_account_pubkey);
+    let fund_y_token_vault_account = token::create_account(&mut client, &fund_vault_authority, &y_token_mint.pubkey());
     println!(
         "fund_y_token_vault_account: {}, {}",
         fund_y_token_vault_account.pubkey(),
@@ -87,8 +134,8 @@ fn main() -> Result<()> {
     token::transfer_to(
         &mut client,
         &initializer_account,
-        &x_token_mint_account_pubkey,
-        &initializer_x_token_account_pubkey,
+        &x_token_mint.pubkey(),
+        &initializer_x_token_account.pubkey(),
         &fund_x_token_vault_account.pubkey(),
         70,
         2,
@@ -96,8 +143,8 @@ fn main() -> Result<()> {
     token::transfer_to(
         &mut client,
         &initializer_account,
-        &y_token_mint_account_pubkey,
-        &initializer_y_token_account_pubkey,
+        &y_token_mint.pubkey(),
+        &initializer_y_token_account.pubkey(),
         &fund_y_token_vault_account.pubkey(),
         30,
         0,
--- fund-tests/src/bin/init_fund.rs
+++ fund-tests/src/bin/init_fund.rs
@@ -23,20 +23,69 @@ use solana_sdk::{
 fn main() -> Result<()> {
     let fund_program_id = Pubkey::from_str("HWEaSAnjKNADwA2ZopatwCM5BqrnMCPQBKAphf1s6XNW")?;
 
-    let initializer_account =
+    let payer_account =
         read_keypair_file("../../solana/alice.json").map_err(|err| anyhow!("Read keypair error: {}", err))?;
 
     let rpc_client = RpcClient::new_with_commitment("http://localhost:8899".into(), CommitmentConfig::confirmed());
     let mut client = Client {
         client: rpc_client,
-        payer: Keypair::from_bytes(&initializer_account.to_bytes()).unwrap(),
+        payer: payer_account,
     };
 
-    let initializer_x_token_account_pubkey = Pubkey::from_str("6F23oWcsyp7D5RkFvi4fha9oUZe1KbK1KvsmDrVS6MeL")?;
-    let initializer_y_token_account_pubkey = Pubkey::from_str("Ho1Y6G3BwMHjxvFw2AVUvcPk9AcyiR6TsR3d72pLu9Me")?;
+    let initializer_account = Keypair::new();
+    println!(
+        "initializer_account: {}, {}",
+        initializer_account.pubkey(),
+        initializer_account.to_base58_string()
+    );
 
-    let x_token_mint_account_pubkey = Pubkey::from_str("ENZSTnpp3FntMj9ApgypoaHY6ZJs8iosyCeFnJj81kpp")?;
-    let y_token_mint_account_pubkey = Pubkey::from_str("5ZTTw1mmjcBzTYB5igjoxUCFAyw8jYczdDgfxWqzV5CS")?;
+    // Create assets
+    let x_token_mint = token::create_token(&mut client, &initializer_account.pubkey(), 2);
+    println!(
+        "x_token_mint: {}, {}",
+        x_token_mint.pubkey(),
+        x_token_mint.to_base58_string()
+    );
+    let y_token_mint = token::create_token(&mut client, &initializer_account.pubkey(), 0);
+    println!(
+        "y_token_mint: {}, {}",
+        y_token_mint.pubkey(),
+        y_token_mint.to_base58_string()
+    );
+
+    // Mint to user
+    let initializer_x_token_account =
+        token::create_account(&mut client, &initializer_account.pubkey(), &x_token_mint.pubkey());
+    println!(
+        "initializer_x_token_account: {}, {}",
+        initializer_x_token_account.pubkey(),
+        initializer_x_token_account.to_base58_string()
+    );
+
+    let initializer_y_token_account =
+        token::create_account(&mut client, &initializer_account.pubkey(), &y_token_mint.pubkey());
+    println!(
+        "initializer_y_token_account: {}, {}",
+        initializer_y_token_account.pubkey(),
+        initializer_y_token_account.to_base58_string()
+    );
+
+    token::mint_to(
+        &mut client,
+        &initializer_account,
+        &x_token_mint.pubkey(),
+        &initializer_x_token_account.pubkey(),
+        1000,
+        2,
+    );
+    token::mint_to(
+        &mut client,
+        &initializer_account,
+        &y_token_mint.pubkey(),
+        &initializer_y_token_account.pubkey(),
+        200,
+        0,
+    );
 
     let fund_name = "Test fund";
 
@@ -68,16 +117,14 @@ fn main() -> Result<()> {
         fund_token_account.to_base58_string()
     );
 
-    let fund_x_token_vault_account =
-        token::create_account(&mut client, &fund_vault_authority, &x_token_mint_account_pubkey);
+    let fund_x_token_vault_account = token::create_account(&mut client, &fund_vault_authority, &x_token_mint.pubkey());
     println!(
         "fund_x_token_vault_account: {}, {}",
         fund_x_token_vault_account.pubkey(),
         fund_x_token_vault_account.to_base58_string()
     );
 
-    let fund_y_token_vault_account =
-        token::create_account(&mut client, &fund_vault_authority, &y_token_mint_account_pubkey);
+    let fund_y_token_vault_account = token::create_account(&mut client, &fund_vault_authority, &y_token_mint.pubkey());
     println!(
         "fund_y_token_vault_account: {}, {}",
         fund_y_token_vault_account.pubkey(),
@@ -88,8 +135,8 @@ fn main() -> Result<()> {
     token::transfer_to(
         &mut client,
         &initializer_account,
-        &x_token_mint_account_pubkey,
-        &initializer_x_token_account_pubkey,
+        &x_token_mint.pubkey(),
+        &initializer_x_token_account.pubkey(),
         &fund_x_token_vault_account.pubkey(),
         70,
         2,
@@ -97,8 +144,8 @@ fn main() -> Result<()> {
     token::transfer_to(
         &mut client,
         &initializer_account,
-        &y_token_mint_account_pubkey,
-        &initializer_y_token_account_pubkey,
+        &y_token_mint.pubkey(),
+        &initializer_y_token_account.pubkey(),
         &fund_y_token_vault_account.pubkey(),
         30,
         0,
